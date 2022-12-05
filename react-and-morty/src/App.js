76a7701f3944eefa5177f1d5148fbc8ca352c632
bmarci888 <bravikmarci@gmail.com>
2022-12-05 19:41:22 +0100

    routing and basic data reading

--- react-and-morty/src/App.js
+++ react-and-morty/src/App.js
@@ -11,7 +11,7 @@ function App() {
   const [planetNumber, setPlanetNumber] = useState(0);
   const [page, setPage] = useState(1);
 
-return  (
+  return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
--- react-and-morty/src/index.js
+++ react-and-morty/src/index.js
@@ -4,12 +4,13 @@ import App from './App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <App />
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
