826f5da3e95f81c7ac6d373baaecaf093af0a777
bmarci888 <bravikmarci@gmail.com>
2022-12-05 19:47:56 +0100

    modify folder routes

--- react-and-morty/src/pages/charactersPage/charactersPage.jsx
+++ react-and-morty/src/pages/charactersPage/charactersPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom"
-import { useCharacters } from "/Users/bravikmarci/codecool/module2/react-and-morty-react-bmarci888/react-and-morty/src/api/useData";
+import { useCharacters } from "../../api/useData";
 import CharacterCards from "./CharacterCards"
 
 export default function LocationsPage() {
