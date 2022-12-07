9f9181567ea6718df61000991aea3ea687f04333
esztivagyok <esztivoltam@gmail.com>
2022-12-07 12:15:06 +0100

    characters css

---
+++ react-and-morty/src/pages/Characters/Character.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useCharacter } from '../../api/useData';
+import './Character.css';
+
+const Character = () => {
+  const { id } = useParams();
+  const characterData = useCharacter(id);
+  console.log(characterData);
+  if (!characterData?.name) {
+    return characterData;
+  }
+
+  const { gender, image, location, name, origin, species, status } =
+    characterData;
+
+  return (
+    <div className="characterPage">
+      <div className="characterCard">
+        <div className='characterInfo'>
+          <img src={image} alt={name} />
+        </div>
+        <div className='characterInfo'>
+          <h1>{name}</h1>
+          <p>Gender: {gender}</p>
+          <p>Status: {status}</p>
+          <p>Species: {species}</p>
+          <p>Location: {location.name}</p>
+          <p>Origin: {origin.name}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Character;
