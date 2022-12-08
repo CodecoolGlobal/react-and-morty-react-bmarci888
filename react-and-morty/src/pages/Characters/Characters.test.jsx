c41e712097482916b20a20c4a46d97150a8328e2
lmesi <lszl.emese@gmail.com>
2022-12-08 11:29:02 +0100

    Delete unwanted file and import

--- react-and-morty/src/pages/Characters/Characters.test.jsx
+++ react-and-morty/src/pages/Characters/Characters.test.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable jest/no-mocks-import */
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { BrowserRouter } from 'react-router-dom';
 import { useCharacters } from '../../api/useData';
--- react-and-morty/src/pages/Locations.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Locations = () => {
-  return <h1>Location</h1>;
-};
-
-export default Locations;
