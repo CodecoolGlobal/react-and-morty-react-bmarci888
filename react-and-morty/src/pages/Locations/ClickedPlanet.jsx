a459212f9cb30c42397fe418574ecc8c4910c4bb
esztivagyok <esztivoltam@gmail.com>
2022-12-08 12:18:18 +0100

    final(?) version

--- react-and-morty/src/pages/Locations/ClickedPlanet.jsx
+++ react-and-morty/src/pages/Locations/ClickedPlanet.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import planetPicture from "../../assets/wallpaperflare.com_wallpaper-2.jpeg"
 import { useLocations } from "../../api/useData";
 import { Link } from "react-router-dom";
+import "./ClickedPlanet.css"
 
 
 export default function ClickedPlanet({ planetNumber, page }) {
@@ -12,16 +13,21 @@ export default function ClickedPlanet({ planetNumber, page }) {
 
     function getDetails(data) {
         const planet = data.results.filter(planet => planet.id === planetNumber);
-        return <div className="planetDetails">
-            <img alt="planet" src={planetPicture} />
-            <div>
-                <h4>{planet[0].name}</h4>
-                <h5>Type: {planet[0].type}</h5>
-                <h5>Dimension: {planet[0].dimension}</h5>
-                <h5>Residents Number: {planet[0].residents.length}</h5>
-                <Link className="backFromDetails" to="/locations">Back To Locations</Link>
+        return <div className="clickedPlanet">
+                    <Link className="backFromDetails" to="/locations">Back To Locations</Link>
+                <div className="clickedCard">
+                    <div className="clickedDetails">
+                        <img alt="planet" src={planetPicture} height="300px"/>
+                    </div>
+                    <div className="clickedDetails">
+                        <h4>{planet[0].name}</h4>
+                        <h5>Type: {planet[0].type}</h5>
+                        <h5>Dimension: {planet[0].dimension}</h5>
+                        <h5>Residents Number: {planet[0].residents.length}</h5>
+
+                    </div>
+                </div>
             </div>
-        </div>
     }
 
     return (
