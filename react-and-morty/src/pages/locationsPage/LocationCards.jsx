0e61a04bbef26a6ca02c90a4772d276c892bff72
bmarci888 <bravikmarci@gmail.com>
2022-12-08 10:34:22 +0100

    finish tests

--- react-and-morty/src/pages/locationsPage/LocationCards.jsx
+++ react-and-morty/src/pages/locationsPage/LocationCards.jsx
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 export default function LocationCards({ useLocations, page, setPlanetNumber }) {
 
     const data = useLocations(page);
+    console.log(data)
 
 
     function cards(locations) {
